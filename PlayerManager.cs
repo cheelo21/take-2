3234c684a90021b3cd2643306aabc409f103ec1a
cheelo21 <88530597+cheelo21@users.noreply.github.com>
2021-08-06 02:34:06 -0500

    some code that almost works
    a l m o s t

---
+++ PlayerManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AN
+{
+    public class PlayerManager : MonoBehaviour
+    {
+        InputHandler inputHandler;
+        Animator anim;
+
+        void Start()
+        {
+            inputHandler = GetComponent<InputHandler>();
+            anim = GetComponent<Animator>();
+        }
+
+
+        void Update()
+        {
+            inputHandler.isInteracting = anim.GetBool("isInteracting");
+
+        }
+    }
+}
